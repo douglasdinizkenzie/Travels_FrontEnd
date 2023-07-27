16a4b922d863e397a625fd89dad8420855928920
Douglas <douglasslycooper@gmail.com>
2023-07-27 15:33:23 -0300

    install framewor chackra ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,32 @@
+import { useNavigate } from "react-router-dom";
 import { headerProps } from "../../interfaces/header/type";
 import "./styles.scss";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 export const Header = ({ optionOne, optionTwo }: headerProps) => {
+  const navigate = useNavigate();
+  const redirect = (options: string) => {
+    if (options == "Login") {
+      navigate("/login");
+    }
+    if (options === "Sign Up") {
+      navigate("/register");
+    }
+    if (options === "Main") {
+      navigate("/");
+    }
+
+    return;
+  };
+
   return (
     <div id="header">
       <p id="header-title">Travels</p>
       <GiHamburgerMenu id="hamburguer" />
       <div id="container-buttons">
-        <p>{optionOne}</p>
+        <p onClick={() => redirect(optionOne)}>{optionOne}</p>
         <div></div>
-        <p>{optionTwo}</p>
+        <p onClick={() => redirect(optionTwo)}>{optionTwo}</p>
       </div>
     </div>
   );
