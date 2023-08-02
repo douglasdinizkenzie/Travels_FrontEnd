1062abae874816d71adf824eab7690fc8159148b
Douglas <douglasslycooper@gmail.com>
2023-08-02 15:49:49 -0300

    feat: finish login page

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,10 @@ import "./styles.scss";
 export const Footer = () => {
   return (
     <div id="footer">
-      <p>© 2023 - Todos os direitos reservados.</p>
+      <p>
+        © 2023 - Todos os direitos reservados - Página criada para fins
+        didáticos!
+      </p>
     </div>
   );
 };
